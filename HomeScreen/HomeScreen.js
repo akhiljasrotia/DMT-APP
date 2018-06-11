9c8a7c5640c0b9ac276cb7d98f55ab630644a5d7
akhiljasrotia <abujasrotia@gmail.com>
2018-06-11 11:52:41 +0530

    Initial Commit

---
+++ HomeScreen/HomeScreen.js
@@ -0,0 +1,39 @@
+
+import React, { Component } from 'react';
+import {
+  Platform,
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  Dimensions
+} from 'react-native';
+
+import Swiper from 'react-native-swiper';
+
+const{height,width}=Dimensions.get('window');
+
+
+
+export default 
+
+const styles = StyleSheet.create(
+{
+
+	mainContainer:{
+		flex:1,
+		backgroundColor:'red',
+	},
+
+	swiper:{
+
+	},
+
+	text1:{
+		
+		fontSize:20,
+
+	},
+
+
+});
